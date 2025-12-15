d792c70646556368a1197bac7f635de317f23ac9
雪グリコシド結晶 <IGCyukira@outlook.com>
2025-12-15 15:26:41 +0800

    Refactor Deno server initialization

--- deno/main.ts
+++ deno/main.ts
@@ -1,13 +1,3 @@
-import { createDenoHandler } from "../src/platforms/deno.ts";
+import { handler } from "../src/platforms/deno.ts";
 
-const denoGlobal = globalThis as {
-	Deno?: { serve?: (handler: (request: Request) => Response | Promise<Response>) => void };
-};
-
-const serve = denoGlobal.Deno?.serve;
-
-if (!serve) {
-	throw new Error("Deno.serve is not available in this environment");
-}
-
-serve(createDenoHandler());
+Deno.serve(handler);
